92f98c7856c7bcd3a86da119f14bd4e73b8a48e1
zakariabijoy <zakaria@vonome.com>
2020-09-27 02:44:43 +0600

    text animation with css and smooth scrolling with jquery

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function () {
+    //smooth scrolling
+
+    $('.btn').on('click', function (e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000);
+
+        }
+    });
+
+});
